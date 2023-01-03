ee11bf69e2f22b7a481ba29b994bfd7642d9e825
Hackdady <lenskii.vm19@physics.msu.ru>
2023-01-04 08:22:11 +0900

    changed names for entries in model

--- models/NFT_Project.py
+++ models/NFT_Project.py
@@ -10,14 +10,14 @@ class Projects(Base):
     __tablename__ = 'nft_pr'
 
     id = Column("id",Integer, primary_key=True, index=True)
-    name = Column("project_name",String(30), nullable=False)
-    project_image= Column("project_image_url",String(100))
-    buy_token_name=Column("buy_token_name",String(5) )
+    name = Column("project_name",String(30), nullable=False) #name of the project
+    project_image_url= Column("project_image_url",String(100))
+    required_token_name=Column("buy_token_name",String(5) )
     chain_name=Column("chain_name",String(30))
-    floor_price=Column("floor_price_BT",Numeric(10,6) )
+    nft_floor_price=Column("floor_price_BT",Numeric(10,6) )
     earn_token_name=Column("earn_token_name",String(5))
     last_updated=Column("last_updated",DateTime)
-    earn_rate_ET=Column("earn_rate_ET",Numeric(10,6))
+    daily_earn_rate_ET=Column("earn_rate_ET",Numeric(10,6))
     
 
 
@@ -38,12 +38,14 @@ class Projects(Base):
         last_updated=Column("last_updated",DateTime)
         twitter=Column("twitter_url",String(100))
         telegram=Column("telegram_url",String(100))
-        site=Column("project_site_url",String(100))
+        webpage=Column("project_site_url",String(100))
         discord=Column("discord_url",String(100))
         project_image=Column("project_image",String(100))
-        floor_price=Column("floor_price_BT",Numeric(10,6) )
-        earn_rate_ET=Column("earn_rate_ET",Numeric(10,6))
+        nft_floor_price=Column("floor_price_BT",Numeric(10,6) )
+        daily_earn_rate_ET=Column("earn_rate_ET",Numeric(10,6))
         category=Column("project_category",String(100))
+        nft_required=Column("nft_number",Integer)
+        project_poster=Column("poster_url",String(100))
 
         def __repr__(self):
             return f'{self.id}: {self.name}: floor price={self.site}'
