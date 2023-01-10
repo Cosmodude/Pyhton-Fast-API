4753fd8d277dbd25ef8a237cc8d14382b10f34d0
Hackdady <lenskii.vm19@physics.msu.ru>
2023-01-10 12:32:39 +0900

    made a function and inserted html body

--- External_API/Mailing_scripts.py
+++ External_API/Mailing_scripts.py
@@ -4,11 +4,36 @@ load_dotenv()
 import os
 from postmarker.core import PostmarkClient
 
-
-postmark = PostmarkClient(server_token= os.getenv('Posmarker_Server_Token'))
-postmark.emails.send(
-    From='contact@crypto-goose.com',
-    To='contact@crypto-goose.com',
-    Subject='Postmark test',
-    HtmlBody='Hi man!'
-)
+def send_email(receiver_email,receiver_name):
+    postmark = PostmarkClient(server_token= os.getenv('Posmarker_Server_Token'))
+    postmark.emails.send(
+        From='contact@crypto-goose.com',
+        To=receiver_email,
+        Subject='Postmark test',
+        HtmlBody= f"<div className='email' style='\
+            border: 1px solid black;\
+            padding: 20px;\
+            font-family: sans-serif;\
+            line-height: 2;\
+            font-size: 20px;\
+            '>\
+            <h2> Hello There, {receiver_name}!</h2>\
+            <p>Test Email</p>\
+            <p>💚, Crypto Goose Team</p>\
+            </div>"\
+     )
+receiver_name = "bill"
+# HtmlBody= f"<div className='email' style='\
+#             border: 1px solid black;\
+#             padding: 20px;\
+#             font-family: sans-serif;\
+#             line-height: 2;\
+#             font-size: 20px;\
+#             '>\
+#             <h2> Hello There, {receiver_name}!</h2>\
+#             <p>Test Email</p>\
+#             <p>💚, Crypto Goose Team</p>\
+#             </div>"\
+                
+#print(HtmlBody)
+#send_email('contact@crypto-goose.com','Vlad')
